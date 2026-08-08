a345060857f05437c77d9979c24ac58eb3cfdca3
Anne-Hélène <ahbricet@gmail.com>
2026-08-08 16:52:14 +0200

    fix shapes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Quizz from "./Quizz";
 
 export default function App() {
@@ -17,46 +17,50 @@ export default function App() {
   }
 
   return (
-    <main>
+    <Fragment>
       <div className="shape yellow-sh"></div>
       <div className="shape blue-sh"></div>
-
-      {start ? (
-        <Quizz difficulty={difficulty} onPlayAgain={resetQuizz} />
-      ) : (
-        <div className="intro-page">
-          <h1>Quizzical 🎯</h1>
-          <p>Test your knowledge !</p>
-          <p>Select a difficulty level to begin the quiz</p>
-          <div className="level">
-            <button
-              className="btn-level btn-easy"
-              onClick={() => startQuizz("easy")}
-            >
-              Easy
-            </button>
-            <button className="btn-level" onClick={() => startQuizz("medium")}>
-              Medium
-            </button>
-            <button
-              className="btn-level btn-hard"
-              onClick={() => startQuizz("hard")}
-            >
-              Hard
-            </button>
+      <main>
+        {start ? (
+          <Quizz difficulty={difficulty} onPlayAgain={resetQuizz} />
+        ) : (
+          <div className="intro-page">
+            <h1>Quizzical 🎯</h1>
+            <p>Test your knowledge !</p>
+            <p>Select a difficulty level to begin the quiz</p>
+            <div className="level">
+              <button
+                className="btn-level btn-easy"
+                onClick={() => startQuizz("easy")}
+              >
+                Easy
+              </button>
+              <button
+                className="btn-level"
+                onClick={() => startQuizz("medium")}
+              >
+                Medium
+              </button>
+              <button
+                className="btn-level btn-hard"
+                onClick={() => startQuizz("hard")}
+              >
+                Hard
+              </button>
+            </div>
+            <footer>
+              Made with ❤️ by{" "}
+              <a
+                target="_blank"
+                href="https://portfolio-anne-helene.netlify.app/"
+              >
+                AnneLn
+              </a>{" "}
+              &copy;2026
+            </footer>
           </div>
-          <footer>
-            Made with ❤️ by{" "}
-            <a
-              target="_blank"
-              href="https://portfolio-anne-helene.netlify.app/"
-            >
-              AnneLn
-            </a>{" "}
-            &copy;2026
-          </footer>
-        </div>
-      )}
-    </main>
+        )}
+      </main>
+    </Fragment>
   );
 }
