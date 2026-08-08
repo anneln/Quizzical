a4092dbe105ab65f629e9d03f3d5fe515bf068e8
Anne-Hélène <ahbricet@gmail.com>
2026-08-08 15:24:26 +0200

    Update Quizz.jsx

--- src/Quizz.jsx
+++ src/Quizz.jsx
@@ -72,6 +72,7 @@ export default function Quizz({ difficulty, onPlayAgain }) {
   }
 
   function playAgain() {
+    window.scrollTo(0, 0); /*pour faire remonter la page*/
     setData([]);
     setScore(0);
     setUserAnswers({});
